8e3f69a77ba12d7064bfb0114ce76adce5e03c94
Chelsiiy <151575298+Chelsiiy@users.noreply.github.com>
2024-07-02 09:48:10 -0700

    Update Homescreen.js

--- Homescreen.js
+++ Homescreen.js
@@ -4,15 +4,17 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function Homescreen() {
   return (
     <View style={styles.container}>
-     <Image source={require('./assets/Logo.png')} style={styles.picture} />
-     <Image source={require('./assets/Menu.png')} style={styles.picture} />
-     <Image source={require('./assets/Filter.png')} style={styles.picture} />
-     <Image source={require('./assets/shoppingBag.png')} style={styles.picture} />
-     <Image source={require('./assets/Listview.png')} style={styles.picture} />
-     <Image source={require('./assets/Search.png')} style={styles.picture} />
-    </View>
-    <View>
-    <Text style={styles.text}> OUR STORY</Text>
+      <View style={styles.imageContainer}>
+        <Image source={require('./assets/Menu.png')} style={styles.picture} />
+        <Image source={require('./assets/Logo.png')} style={styles.picture} />
+        <Image source={require('./assets/Search.png')} style={styles.picture} />
+        <Image source={require('./assets/shoppingBag.png')} style={styles.picture} />
+      </View>
+      <View>
+        <Text style={styles.text}>OUR STORY</Text>
+         <Image source={require('./assets/Filter.png')} style={styles.picture} />
+          <Image source={require('./assets/Listview.png')} style={styles.picture} />
+      </View>
     </View>
   );
 }
@@ -20,9 +22,18 @@ export default function Homescreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
+  },
+  imageContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  picture: {
+    width: 50,
+    height: 50,
+    margin: 5,
   },
   text: {
     fontSize: 20,
