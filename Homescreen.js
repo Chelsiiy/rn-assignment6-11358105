9739fe183c26b21a942eb9aaa562046c357eab10
Chelsiiy <151575298+Chelsiiy@users.noreply.github.com>
2024-07-03 07:59:05 -0700

    Update Homescreen.js

--- Homescreen.js
+++ Homescreen.js
@@ -5,58 +5,58 @@ export default function Homescreen() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={require('./assets/Logo.png')} style={styles.picture} />
-        <Image source={require('./assets/Menu.png')} style={styles.picture} />
-        <Image source={require('./assets/shoppingBag.png')} style={styles.picture} />
-        <Image source={require('./assets/Search.png')} style={styles.picture} />
+       <Image source={require('./assets/Menu.png')} style={styles.icon} />
+        <Image source={require('./assets/Logo.png')} style={styles.icon} />
+        <Image source={require('./assets/shoppingBag.png')} style={styles.icon} />
+        <Image source={require('./assets/Search.png')} style={styles.icon} />
       </View>
-      <View>
+      <View style={styles.storyContainer}>
         <Text style={styles.text}>OUR STORY</Text>
-         <Image source={require('./assets/Filter.png')} style={styles.picture} />
-          <Image source={require('./assets/Listview.png')} style={styles.picture} />
+        <Image source={require('./assets/Filter.png')} style={styles.picture} />
+        <Image source={require('./assets/Listview.png')} style={styles.picture} />
       </View>
-      <Scrollview vertical>
-       <View>
-         <Image source={require('./assets/dress-1.png')} style={styles.picture} />
-         <Text style={styles.text}>OFFICE WEAR</Text>
-          <Text style={styles.text}>reversible angora cardigan</Text>
-      </View>
-         <View>
-         <Image source={require('./assets/dress-2.png')} style={styles.picture} />
-         <Text style={styles.text}>BLACK</Text>
-          <Text style={styles.text}>reversible angora cardigan</Text>
-      </View>
-         <View>
-         <Image source={require('./assets/dress-3.png')} style={styles.picture} />
-         <Text style={styles.text}>CHURCH WEAR</Text>
-          <Text style={styles.text}>reversible angora cardigan</Text>
-      </View>
-         <View>
-         <Image source={require('./assets/dress-4.png')} style={styles.picture} />
-         <Text style={styles.text}>LAMEREI</Text>
-          <Text style={styles.text}>reversible angora cardigan</Text>
-      </View>
-         <View>
-         <Image source={require('./assets/dress-5.png')} style={styles.picture} />
-         <Text style={styles.text}>21WN</Text>
-          <Text style={styles.text}>reversible angora cardigan</Text>
-      </View>
-         <View>
-         <Image source={require('./assets/dress-6.png')} style={styles.picture} />
-         <Text style={styles.text}>LOPO</Text>
-          <Text style={styles.text}>reversible angora cardigan</Text>
-      </View>
-         <View>
-         <Image source={require('./assets/dress-7.png')} style={styles.picture} />
-         <Text style={styles.text}>21WN</Text>
-          <Text style={styles.text}>reversible angora cardigan</Text>
-      </View>
-       <View>
-         <Image source={require('./assets/sundress.jpg')} style={styles.picture} />
-         <Text style={styles.text}>SUNDRESS</Text>
-          <Text style={styles.text}>reversible angora cardigan</Text>
-      </View>
-      </Scrollview>
+      <ScrollView>
+        <View style={styles.itemContainer}>
+          <Image source={require('./assets/dress-1.png')} style={styles.dress} />
+          <Text style={styles.itemTitle}>OFFICE WEAR</Text>
+          <Text style={styles.itemDescription}>Reversible Angora Cardigan</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Image source={require('./assets/dress-2.png')} style={styles.gown} />
+          <Text style={styles.itemTitle}>BLACK</Text>
+          <Text style={styles.itemDescription}>Reversible Angora Cardigan</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Image source={require('./assets/dress-3.png')} style={styles.dress} />
+          <Text style={styles.itemTitle}>CHURCH WEAR</Text>
+          <Text style={styles.itemDescription}>Reversible Angora Cardigan</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Image source={require('./assets/dress-4.png')} style={styles.gown} />
+          <Text style={styles.itemTitle}>LAMEREI</Text>
+          <Text style={styles.itemDescription}>Reversible Angora Cardigan</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Image source={require('./assets/dress-5.png')} style={styles.dress} />
+          <Text style={styles.itemTitle}>21WN</Text>
+          <Text style={styles.itemDescription}>Reversible Angora Cardigan</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Image source={require('./assets/dress-6.png')} style={styles.gown} />
+          <Text style={styles.itemTitle}>LOPO</Text>
+          <Text style={styles.itemDescription}>Reversible Angora Cardigan</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Image source={require('./assets/dress-7.png')} style={styles.dress} />
+          <Text style={styles.itemTitle}>21WN</Text>
+          <Text style={styles.itemDescription}>Reversible Angora Cardigan</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Image source={require('./assets/sundress.jpg')} style={styles.gown} />
+          <Text style={styles.itemTitle}>SUNDRESS</Text>
+          <Text style={styles.itemDescription}>Reversible Angora Cardigan</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -69,16 +69,51 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flexDirection: 'row',
-    flexWrap: 'wrap',
     justifyContent: 'center',
+    marginTop: 20,
+  },
+  storyContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  icon: {
+    margin: 5,
+    marginRight:15,
+    marginLeft:15,
   },
   picture: {
-    width: 50,
-    height: 50,
-    margin: 5,
+    width: 30,
+    height: 30,
+    marginLeft: 5,
+    marginRight: 45,
   },
   text: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginRight: 30,
+  },
+  itemContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  dress: {
+    width: 200,
+    height: 250,
+    marginRight:250,
+  },
+  gown:{
+    width: 200,
+    height: 250,
+    marginLeft:200,
+  },
+  itemTitle: {
     fontSize: 20,
-    marginBottom: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  itemDescription: {
+    fontSize: 16,
+    marginTop: 5,
   },
 });
