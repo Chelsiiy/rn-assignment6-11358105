dfb569c808377a6fa2a1180d50e710d1e35a4a41
Chelsiiy <151575298+Chelsiiy@users.noreply.github.com>
2024-07-03 15:20:49 -0700

    Update Homescreen.js

--- Homescreen.js
+++ Homescreen.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, Button } from 'react-native';
 
-export default function Homescreen() {
-  const [cart, setCart] = useState([]);
-
+export default function Homescreen({ navigation, cart, setCart }) {
   const addToCart = (item) => {
     setCart([...cart, item]);
     alert(`${item.title} added to cart!`);
@@ -48,6 +46,9 @@ export default function Homescreen() {
           ))}
         </View>
       </ScrollView>
+      <TouchableOpacity style={styles.cartButton} onPress={() => navigation.navigate('Cart', { cart, setCart })}>
+        <Text style={styles.cartButtonText}>Go to Cart ({cart.length})</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -135,5 +136,16 @@ const styles = StyleSheet.create({
   addButtonText: {
     color: '#fff',
     fontSize: 20,
+  },
+  cartButton: {
+    backgroundColor: '#000',
+    borderRadius: 5,
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  cartButtonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
